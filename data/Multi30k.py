1f2a290a20f06e995023ec3bc4fd28eaefcadad7
mhannani <mohamed_hanani2019@gmail.com>
2021-12-19 21:35:57 +0100

    Vocabulary builer

--- data/Multi30k.py
+++ data/Multi30k.py
@@ -1,6 +1,4 @@
 from torchtext.datasets import Multi30k
-from torchtext.data.utils import get_tokenizer
-from torchtext.vocab import build_vocab_from_iterator
 from torch.utils.data import DataLoader
 from config import *
 
@@ -20,53 +18,9 @@ class CustomMulti30k:
 
         self.train, self.valid, self.test = Multi30k(root="../.data", split=split, language_pair=language_pair)
 
-    def _get_tokenizer(self, lang):
+    def extract_sets(self):
         """
-        Get the spacy tokenizer for the lang language.
-        :param lang: str
-            'en' for English or 'de' for Dutch.
-        :return: spacy.tokenizer
-        """
-
-        if lang not in LANG_SHORTCUTS:
-            raise ValueError('Language not found')
-        else:
-            return get_tokenizer('spacy', language=LANG_SHORTCUTS[lang])
-
-    def _get_tokens(self, data_iterator=None, lang='en'):
-        """
-        Get token for an iterator containing tuple of string
-        :param lang: str
-            'en' or 'de' for source and target languages.
-        :return: List
-            List of tokens.
-        """
-        for data_sample in data_iterator:
-            yield self._get_tokenizer(lang)(data_sample[LANGUAGE_INDEX[lang]])
-
-    def build_vocab(self, data_iterator, lang='en'):
-        """
-        Build the vocabulary of the given language.
-        :param data_iterator: Iterator
-            Data iterator.
-        :param lang: str
-            Language shortcut.
-        :return: None
-        """
-
-        # for English lang
-        if lang in LANG_SHORTCUTS:
-            vocabulary = build_vocab_from_iterator(self._get_tokens(data_iterator, lang), min_freq=1,
-                                      specials=SPECIAL_SYMBOLS, special_first=True)
-            vocabulary.set_default_index(UNK_IDX)
-
-            return vocabulary
-        else:
-            raise ValueError('Not a supported language')
-
-    def _extract_sets(self):
-        """
-        Extracts train, valid and test sets
+        Extracts train, valid and test sets from Multi30K dataset.
         :return: List
         """
         return self.train, self.valid, self.test
@@ -74,7 +28,7 @@ class CustomMulti30k:
 
 if __name__ == "__main__":
     custom_multi30k = CustomMulti30k(split=('train', 'valid', 'test'))
-    train, valid, test = custom_multi30k._extract_sets()
+    train, valid, test = custom_multi30k.extract_sets()
     print('DATASET SUMMARY')
     print('+++++++++++++++')
     print(f'+ Train test: {train.__len__()} sentences')
--- data/Vocabulary.py
+++ data/Vocabulary.py
@@ -1,5 +1,58 @@
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+from config import *
+
+
 class Vocabulary:
     """
     Build vocabulary for a language.
     """
-    pass
+
+    def _get_tokenizer(self, lang):
+        """
+        Get the spacy tokenizer for the lang language.
+        :param lang: str
+            'en' for English or 'de' for Dutch.
+        :return: spacy.tokenizer
+        """
+
+        if lang not in LANG_SHORTCUTS:
+            raise ValueError('Language not found')
+        else:
+            return get_tokenizer('spacy', language=LANG_SHORTCUTS[lang])
+
+    def _get_tokens(self, data_iterator=None, lang='en'):
+        """
+        Get token for an iterator containing tuple of string
+        :param lang: str
+            'en' or 'de' for source and target languages.
+        :return: List
+            List of tokens.
+        """
+        for data_sample in data_iterator:
+            yield self._get_tokenizer(lang)(data_sample[LANGUAGE_INDEX[lang]])
+
+    def build_vocab(self, data_iterator, lang='en'):
+        """
+        Build the vocabulary of the given language.
+        :param data_iterator: Iterator
+            Data iterator.
+        :param lang: str
+            Language shortcut.
+        :return: None
+        """
+
+        # for English lang
+        if lang in LANG_SHORTCUTS:
+            vocabulary = build_vocab_from_iterator(self._get_tokens(data_iterator, lang), min_freq=1,
+                                                   specials=SPECIAL_SYMBOLS, special_first=True)
+            vocabulary.set_default_index(UNK_IDX)
+
+            return vocabulary
+        else:
+            raise ValueError('Not a supported language')
+
+
+if __name__ == "__main__":
+    vocab = Vocabulary()
+
