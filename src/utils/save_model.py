d05e8054915b79d94114e6eaffd8d5228578c45b
mhannani <mohamed_hanani2019@gmail.com>
2021-12-25 18:41:37 +0100

    Training process

--- src/utils/save_model.py
+++ src/utils/save_model.py
@@ -1,18 +1,20 @@
 from torch import save
 
 
-def save_model(model, src_vocabulary, tgt_vocabulary, filename):
+def save_model(model, src_vocabulary, tgt_vocabulary, epoch, filename):
     """
     Save trained model, along with source and target languages vocabularies.
     :param model: trained model
     :param src_vocabulary: source language vocabulary.
     :param tgt_vocabulary: target language vocabulary.
+    :param epoch: epoch at which the model will be saved.
     :param filename: filename of the checkpoint.
     :return: None
     """
 
     # define checkpoint dictionary
     checkpoint = {
+        'epoch': epoch + 1,
         'src_vocabulary': src_vocabulary,
         'tgt_vocabulary': tgt_vocabulary,
         'model_state_dict': model.state_dict()
--- train.py
+++ train.py
@@ -69,7 +69,7 @@ def train(train_iter, valid_iter, src_vocab, tgt_vocab, epochs=EPOCHS):
             train_loss.append(loss.item())
 
             # update the progress bar
-            p_bar.set_postfix(epoch=f" {epoch}, train loss= {round(sum(train_loss) / len(train_loss), 4)}, valid loss = +inf",
+            p_bar.set_postfix(epoch=f" {epoch}/{EPOCHS}, train loss= {round(sum(train_loss) / len(train_loss), 4)}, valid loss = +inf",
                              refresh=True)
             p_bar.update()
 
@@ -98,19 +98,20 @@ def train(train_iter, valid_iter, src_vocab, tgt_vocab, epochs=EPOCHS):
                 valid_loss.append(loss.item())
 
         p_bar.set_postfix(
-            epoch=f" {epoch}, train loss= {round(sum(train_loss) / len(train_loss), 4)}, valid loss = {round(sum(train_loss) / len(train_loss), 4)}",
+            epoch=f" {epoch}/{EPOCHS}, train loss= {round(sum(train_loss) / len(train_loss), 4)}, valid loss = {round(sum(train_loss) / len(train_loss), 4)}",
             refresh=False)
         p_bar.close()
 
         # Save the checkpoint
-        # save_model(seq2seq, src_vocab, tgt_vocab, f'checkpoints/CHECKPOINT_WITHOUTO_ATT__EN__TO__DE__EPOCH_{epoch}__AT__{strftime("%Y_%m_%d__%H_%M_%S", gmtime())}__TRAIN_LOSS__{round(sum(training_loss) / len(training_loss))}')
+        if train_loss[-1] < train_loss[-2]:
+            save_model(seq2seq, src_vocab, tgt_vocab, epoch=epoch, filename=f'checkpoints/CHECKPOINT_WITHOUT_ATT__EN__TO__DE__EPOCH_{epoch}__AT__{strftime("%Y_%m_%d__%H_%M_%S", gmtime())}__TRAIN_LOSS__{round(sum(train_loss) / len(train_loss))}')
 
 
 if __name__ == "__main__":
     print('Training...')
 
     # Getting train DataLoader
-    train_iterator = get_data(root='data/.data', batch_size=BATCH_SIZE, split='test')
+    train_iterator = get_data(root='data/.data', batch_size=BATCH_SIZE, split='train')
 
     # Getting valid DataLoaders
     valid_iterator = get_data(root='data/.data', batch_size=BATCH_SIZE, split='valid')
